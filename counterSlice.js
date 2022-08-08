3ec82713dc0e1c64c7850aa2800c4415a6d1ce19
Arunkumar S <104545860+arunkumaa6@users.noreply.github.com>
2022-08-08 23:34:32 +0530

    Add files via upload

---
+++ counterSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from '@reduxjs/toolkit';
+// import { fetchCount } from './counterAPI';
+
+export const counterSlice = createSlice({
+  name: 'counter',
+  initialState:{
+    cartItem:[],
+    favorite:[]
+  },
+  
+  reducers: {
+
+    addToCart:(state,action) => {
+      console.log("state",state,action);
+      // state.value =+ action.payload
+      
+    },
+    
+  },
+});
+
+export const { addToCart } = counterSlice.actions;
+
+export default counterSlice.reducer;
